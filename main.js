842c4ce1b5f6f4ca363aa1084578b492e9e6242a
tijanagv <tijanagvozdic.tkgv@gmail.com>
2021-03-21 00:10:29 +0100

    Full project commit

---
+++ main.js
@@ -0,0 +1,7 @@
+
+let icon = document.getElementById("hamburger-menu")
+let navigation = document.getElementById("navigation")
+icon.addEventListener("click", function() {
+  navigation.classList.toggle('show')
+}
+)
